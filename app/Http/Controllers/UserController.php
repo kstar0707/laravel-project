4c9d04d8c707e0619425b955af0426cf5f41c599
kstar0102 <kstar0101@outlook.com>
2023-12-12 02:05:19 +1000

    updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
+    public function boot()
+    {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+    }
     /**
      * Display a listing of the resource.
      */
